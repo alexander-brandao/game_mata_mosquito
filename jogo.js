4ea716c29a995c6d130580ea54cb59af6adb58f6
Alexander Brandao <alex12a7x@gmail.com>
2022-01-29 18:20:40 -0300

    posicao dinamica

--- jogo.js
+++ jogo.js
@@ -7,3 +7,25 @@ function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
 }
+
+ajustaTamanhoPalcoJogo()
+// definindo posicao dinamica do mosquito
+
+function posicaoRandomica(){
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    //criar elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
