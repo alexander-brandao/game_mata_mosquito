dea9fdbb5c1a564a96202feea2a1ee21a4c4f0bb
Alexander Brandao <alex12a7x@gmail.com>
2022-02-04 04:43:38 -0300

    controlando pontos de vida

--- jogo.js
+++ jogo.js
@@ -2,6 +2,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -16,6 +17,13 @@ function posicaoRandomica(){
     //remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3){
+            alert('game over')
+        }else{
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -32,6 +40,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id= "mosquito"
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 }
